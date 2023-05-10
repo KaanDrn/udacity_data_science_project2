5ec1de71104e70da8290764226fddbd9f5511208
Duran <k35400@eon.com>
2023-05-10 17:16:37 +0200

    modified flask app to feed my model/data

--- app/run.py
+++ app/run.py
@@ -1,56 +1,44 @@
 import json
+import pickle
+
 import plotly
-import pandas as pd
-
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
 from flask import Flask
-from flask import render_template, request, jsonify
+from flask import render_template, request
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
-from sqlalchemy import create_engine
 
+from tools import ml_tools
 
 app = Flask(__name__)
 
-def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
-    return clean_tokens
-
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+data_obj = ml_tools.TrainingPreprocessor("../resources/DisasterResponse.db")
+data_obj.connect_to_db()
+data_obj.load_data()
+data_obj.connection.close()
+df = data_obj.data
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+with open("../resources/tweet_classifier.pkl", "rb") as file:
+    model = pickle.load(file=file)
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
             'data': [
                 Bar(
-                    x=genre_names,
-                    y=genre_counts
+                        x=genre_names,
+                        y=genre_counts
                 )
             ],
 
@@ -65,11 +53,11 @@ def index():
             }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -78,17 +66,17 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
-    classification_labels = model.predict([query])[0]
+    classification_labels = model.predict([query])[0][4:].astype(int)
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
-        'go.html',
-        query=query,
-        classification_result=classification_results
+            'go.html',
+            query=query,
+            classification_result=classification_results
     )
 
 
